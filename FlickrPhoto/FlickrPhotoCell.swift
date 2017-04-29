bf3691933a8e0909042a79493b9d6f0dacb9f8c1
Duy Bùi <duybui@Duys-Mac.local>
2017-04-29 15:52:37 +0700

    First commit

---
+++ FlickrPhoto/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrPhoto
+//
+//  Created by Duy Bùi on 4/29/17.
+//  Copyright © 2017 Duy Bùi. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
